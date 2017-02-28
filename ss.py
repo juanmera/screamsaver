f1147cdfe6daacbc289ce2c0beb9d304c894c688
Juan <juanm@avature.net>
2017-02-27 21:52:00 -0300

    Thanks @josecanciani for unmute bugfix

--- ss.py
+++ ss.py
@@ -1,4 +1,5 @@
 import Quartz
+import os
 import time
 from playsound import playsound
 
@@ -6,6 +7,7 @@ while True:
     gc = Quartz.CGSessionCopyCurrentDictionary(); 
     if 'CGSSessionScreenIsLocked' in gc and gc['CGSSessionScreenIsLocked'] == 1:
         if 'kCGSSessionSecureInputPID' in gc and gc['CGSSessionScreenIsLocked'] > 0:
+            os.system('osascript -e "set volume output volume 100"')  
             playsound('scream.wav')
     time.sleep(0.1)
 
